d459c2c458caa86a9990f4dcc8d1f38a57fd23e1
Stephen Johnson <stephenjjohnson@gmail.com>
2016-04-25 09:22:46 -0500

    Add GPL-3 license

--- PE_functions.py
+++ PE_functions.py
@@ -1,5 +1,18 @@
-# Stephen Johnson
-# Functions for oil and gas calculations
+#    PE_functions.py provides functions for oil and gas calculations
+#    Copyright (C) 2016 Stephen J. Johnson
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.# Stephen Johnson
 
 #import math
 
